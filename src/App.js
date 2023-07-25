ff7c2373e661e9aba21287adaa5aa4abc81e0d48
Eric <ericdelmermillen@gmail.com>
2023-07-25 07:04:07 -0400

    finished new mixins for typography, buttons and padding

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ function App() {
       <h2>Message After Setting Fonts</h2>
       <h3>Message After Creating feature-variables branch off of develop</h3>
       <h4>Message After creating feature-mixins</h4>
+      <button>Click Me</button>
+      <button>Mixins Commit</button>
     </div>
   );
 }
