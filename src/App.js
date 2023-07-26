71d5721292bddb08160857c3dc7cf91f269b4a82
Eric Delmer Millen <ericdelmermillen@gmail.com>
2023-07-26 10:14:25 -0400

    commit before creating feature-avatar branch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import "./App.scss";
 import "./styles/partials/_global.scss"
 import "./styles/partials/_variables.scss";
-// import Button from "./components/Components/Button";
-import Header from "./components/Components/Header"
-// import uploadIcon from "./assets/images/Icons/upload.svg"
+import Header from "./components/Components/Header";
 
 // import commentIcon from "./assets/images/Icons/add_comment.svg"
 
