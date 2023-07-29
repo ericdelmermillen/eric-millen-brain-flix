1a537771f5e1d24be9c51391af59d4b94feeb6af
Eric <ericdelmermillen@gmail.com>
2023-07-28 22:36:24 -0400

    added styling for HeroDetails at mobile width

--- src/components/HeroDetails/HeroDetails.js
+++ src/components/HeroDetails/HeroDetails.js
@@ -1,6 +1,5 @@
 import "./HeroDetails.scss"; 
 
-
 function HeroDetails({currentVideo, viewsIcon, likesIcon}) {
   const {title, channel, description, views, likes, comments, timestamp} = currentVideo;
 
@@ -14,29 +13,31 @@ function HeroDetails({currentVideo, viewsIcon, likesIcon}) {
   return (
     
     <section className="hero__details">
+
       <h2 className="hero__heading">{title}</h2>
 
-    <div className="hero__stats">
-      <header className="hero__header">
-        <h3>By {channel}</h3>
-        <p>{date}</p>
-      </header>
+      <div className="hero__stats">
+        <header className="hero__header">
+          <p className="hero__header-channel">By {channel}</p>
+          <p className="hero__header-date">{date}</p>
+        </header>
 
-      <div className="hero__views">
-        <img src={viewsIcon} alt="" />
-        <p>{views}</p>
+        <div className="hero__numbers">
+          <div className="hero__views">
+            <img className="hero__icon" src={viewsIcon} alt="" />
+            <p>{views}</p>
+          </div>
+          <div className="hero__likes">
+            <img className="hero__icon" src={likesIcon} alt="" />
+            <p>{likes}</p>
+          </div>
+        </div>
+
       </div>
 
-      <div className="hero__likes">
-        <img src={likesIcon} alt="" />
-        <p>{likes}</p>
-      </div>
-    </div>
-    <p className="hero__description">{description}</p>
-    <p className="hero__comment-count">{comments.length} Comments</p> 
-
-  </section>
-  )
-}
+      <p className="hero__description">{description}</p>
+      <p className="hero__comment-count">{comments.length} Comments</p> 
+    </section>
+  )}
 
 export default HeroDetails;
